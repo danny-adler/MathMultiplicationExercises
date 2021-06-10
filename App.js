97a02f6947358f7e9c7fd4e29f017b68a7d17f57
Danny Adler <adler.danny@gmail.com>
2021-06-10 21:49:55 +0300

    force LTR

--- App.js
+++ App.js
@@ -3,14 +3,15 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { I18nManager } from 'react-native';
 import ExerciseList from './ExerciseList';
 
-try {
-  I18nManager.allowRTL(false);
-}
-catch (e) {
-  console.log(e);
-}
-
 export default function App() {
+  try {
+    I18nManager.allowRTL(false);
+    I18nManager.forceRTL(false);
+  }
+  catch (e) {
+    console.log(e);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={{ paddingTop: 30, textAlign: "center", fontSize: 28 }}>ענבר המושלמת</Text>
